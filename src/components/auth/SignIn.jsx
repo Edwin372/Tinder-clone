e139ce696756fd3b1ab4f9092fec7955d2e8cc08
Châu <tranhoaichau.00@gmail.com>
2020-10-25 14:48:30 +0700

    add font

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -4,7 +4,8 @@ import { signIn } from '../../store/actions/authAction'
 import { Redirect } from 'react-router-dom'
 import StyledInput from '../inputs/StyledInput.jsx'
 import StyledButton from '../buttons/StyledButton.jsx'
-import bigLogo from '../../svg/big-logo.svg'
+import bigLogo from '../../svg/bigLogo.svg'
+import smallLogo from '../../svg/smallLogo.svg'
 import SocialAccountBtn from '../buttons/SocialAccountBtn.jsx'
 import './SignIn.scss'
 class SignIn extends Component {
@@ -25,11 +26,12 @@ class SignIn extends Component {
     const { authError, auth } = this.props;
     if (auth.uid) return <Redirect to='/' /> 
     return (
-      <div className="container">
-        <img src={bigLogo} alt="big-logo"/>
-        <div className="form-container">
+        <div className="container">
+          <img src={smallLogo} alt="small-logo" id="small-logo"/>
+          <img src={bigLogo} alt="big-logo"/>
+          <div className="form-container">
           <form className="white" onSubmit={this.handleSubmit}>
-            <StyledInput label="User name/Email" type="email" id='email' onChange={this.handleChange}  />
+            <StyledInput label="Email" type="email" id='email' onChange={this.handleChange}  />
             <StyledInput label="Password" type="password" id='password' onChange={this.handleChange}  />
             <StyledButton text="Login"/>
               <div className="center red-text">
@@ -38,10 +40,8 @@ class SignIn extends Component {
           </form>
           <SocialAccountBtn onClick={() => {}} id={"facebook-btn"} />
           <SocialAccountBtn onClick={() => {}} id={"google-btn"}/>
-
-        </div>
-       
-      </div>
+           </div>
+         </div>
     )
   }
 }
