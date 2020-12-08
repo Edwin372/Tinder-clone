99f87c5fb9de9682668a1cb86a9cc23398a1e525
An D Van <vdan18@apcs.vn>
2020-12-08 15:21:30 +0700

    add logic reading content

--- src/components/layout/ReadingContent.jsx
+++ src/components/layout/ReadingContent.jsx
@@ -5,6 +5,7 @@ import "./ReadingContent.scss";
 import testImage from "../../images/testImage1.jpeg";
 
 const ReadingContent = (props) => {
+  const { profile, post } = props;
   return (
     <div id="reading-content-container">
       <div id="reading-content-header">
@@ -12,31 +13,35 @@ const ReadingContent = (props) => {
           <img id="save-icon" src={saveIcon} alt="saveIcon"></img>
         </button>
         <div id="author-info">
-          <img id="author-ava" src={defaultImage}></img>
+          <img id="author-ava" src={profile.avatar}></img>
           <div id="author-name-date">
-            <a id="author-name">Author's Name</a>
-            <p id="post-date">April 18th,2020</p>
+            <a id="author-name">{profile.displayName}</a>
+            <p id="post-date">{post.postDate}</p>
           </div>
         </div>
       </div>
       <div id="reading-content-preheader">
-        <p id="reading-content-title">Title</p>
-        <p id="reading-content-subtitle">Subtitle</p>
+        <p id="reading-content-title">{post.title}</p>
+        <p id="reading-content-subtitle">{post.subtitle}</p>
       </div>
-      <img id="reading-content-image" src={testImage} alt="title-image" />
+      <img
+        className="reading-content-image"
+        src={post.image}
+        alt="title-image"
+      />
+      <p className="reading-content-body-title"></p>
+      <p className="reading-content-body-paragraph"></p>
       <div id="reading-content-body">
-        <p className="reading-content-body-title">Lorem Ipsum là gì?</p>
-        <p className="reading-content-body-paragraph">
-          Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc
-          trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng
-          như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500,
-          khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành
-          một bản mẫu văn bản. Đoạn văn bản này không những đã tồn tại năm thế
-          kỉ, mà khi được áp dụng vào tin học văn phòng, nội dung của nó vẫn
-          không hề bị thay đổi. Nó đã được phổ biến trong những năm 1960 nhờ
-          việc bán những bản giấy Letraset in những đoạn Lorem Ipsum, và gần đây
-          hơn, được sử dụng trong các ứng dụng dàn trang, như Aldus PageMaker.
-        </p>
+        {post.contents.map((item, index) => (
+          <div key={index}>
+            <p className="reading-content-body-title" key={index}>
+              {item.title}
+            </p>
+            {item.content.map((pItem, index) => (
+              <p className="reading-content-body-paragraph">{pItem}</p>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
