89ca507c4d9d226e77aa1f0beb4cc759f1d49386
An D Van <vdan18@apcs.vn>
2020-12-10 15:58:20 +0700

    Add image to comment

--- src/components/comments/InputComment.jsx
+++ src/components/comments/InputComment.jsx
@@ -1,44 +1,64 @@
-import React, { Component} from 'react'
-import './InputComment.scss'
-import TextareaAutosize from 'react-autosize-textarea'
-import commentIcon from '../../svg/commentLogo.svg'
-import addImgIcon from '../../svg/addImgaeIcon.svg'
-import defaultimg from '../../images/defaultImage.png'
-import defaultAvatar from '../../images/defaultAvatar.png'
+import React, { useState } from "react";
+import "./InputComment.scss";
+import TextareaAutosize from "react-autosize-textarea";
+import commentIcon from "../../svg/commentLogo.svg";
+import addImgIcon from "../../svg/addImgaeIcon.svg";
+import defaultimg from "../../images/defaultImage.png";
+import defaultAvatar from "../../images/defaultAvatar.png";
+import ReactFileReader from "react-file-reader";
 
-
-export default function InputComment({user, handlePostNewComment}) {
-    function handleEnterPress(e) {
-        let key = window.event.keyCode;
-        let isShift = !!window.event.shiftKey;
-        
-        if (key === 13 && !isShift && e.target.value !== '') {
-            e.preventDefault();
-            handlePostNewComment(e);
-            e.target.value = ''
-        }
-       
+export default function InputComment({ user, handlePostNewComment }) {
+  const [image, setImage] = useState("");
+  function handleEnterPress(e, image) {
+    let key = window.event.keyCode;
+    let isShift = !!window.event.shiftKey;
+    if (key === 13 && !isShift && (e.target.value !== "" || image !== "")) {
+      e.preventDefault();
+      handlePostNewComment(e, image);
+      e.target.value = "";
+      setImage("");
     }
-    return (
-        <div className="input-comment">
-            <div className="line"></div>
-            <div className="comment-logo">
-                    <h1 className="comment-text">Comments</h1>
-                    <img src={commentIcon} alt="comment-icon"></img>
-            </div>
-            <div className="input-of-user">
-                <div className="ava-and-name">
-                    <img className="comment-avatar" src={user.avatar || defaultAvatar} alt="avatar" ></img>
-                    <p>{user.name}</p>
-                </div>
-                <div className="textarea-and-addImageIcon">
-                    <TextareaAutosize className="input-text" placeholder="Write your comment here ..." onKeyPress={(e) => handleEnterPress(e)}></TextareaAutosize>
-                    <button id ="comment-image-btn">
-                        <img src={addImgIcon} id="image-btn"></img>
-                    </button> 
-                 
-                </div>
-            </div>
+  }
+  function handleFiles(files) {
+    console.log(files.base64);
+    setImage(files.base64);
+  }
+  return (
+    <div className="input-comment">
+      <div className="line"></div>
+      <div className="comment-logo">
+        <h1 className="comment-text">Comments</h1>
+        <img src={commentIcon} alt="comment-icon"></img>
+      </div>
+      <div className="input-of-user">
+        <div className="ava-and-name">
+          <img
+            className="comment-avatar"
+            src={user.avatar || defaultAvatar}
+            alt="avatar"
+          ></img>
+          <p>{user.name}</p>
         </div>
-    )
+        <div className="textarea-and-addImageIcon">
+          <TextareaAutosize
+            className="input-text"
+            placeholder="Write your comment here ..."
+            onKeyPress={(e) => handleEnterPress(e, image)}
+          ></TextareaAutosize>
+          <ReactFileReader
+            fileTypes={[".png", ".jpg", "jpeg"]}
+            base64={true}
+            handleFiles={handleFiles}
+          >
+            <button className="btn" id="comment-image-btn">
+              <img src={addImgIcon} id="image-btn"></img>
+            </button>
+          </ReactFileReader>
+        </div>
+        {image ? (
+          <img className="preview-img" src={image} alt="preview-image" />
+        ) : null}
+      </div>
+    </div>
+  );
 }
--- src/components/comments/UserComment.jsx
+++ src/components/comments/UserComment.jsx
@@ -1,32 +1,33 @@
-import React, { Component} from 'react'
-import TextareaAutosize from 'react-autosize-textarea'
-import './UserComment.scss'
-import likeBtnIcon from '../../svg/likeButton.svg'
+import React, { Component } from "react";
+import TextareaAutosize from "react-autosize-textarea";
+import "./UserComment.scss";
+import likeBtnIcon from "../../svg/likeButton.svg";
 
 export default function UserComment(props) {
-    return (
-        <div className="user-comment">
-            <div className="user-name-date">
-                <img className="comment-avatar" src={props.avatar} alt="avatar"></img>
-                <div className="name-date">
-                    <p className="name"> {props.name} </p>
-                    <p className="date"> {props.dateComment} </p>
-                </div>
-            </div>
-            <div className="comment-content-container">
-                <div className="comment-content">
-                    <div className="text-style">{
-                      props.commentContent.split('\n').map((line, index) => <p key={index}>{line}</p>)
-                    }</div>
-
-                </div>
-                <button className="like-btn">
-                        <img src={likeBtnIcon} alt=""/>
-                </button>
-            </div>
-            
-            {props.commentImage ? <img src = {props.commentImage} className="comment-img"></img>: null}
-           
+  return (
+    <div className="user-comment">
+      <div className="user-name-date">
+        <img className="comment-avatar" src={props.avatar} alt="avatar"></img>
+        <div className="name-date">
+          <p className="name"> {props.name} </p>
+          <p className="date"> {props.dateComment} </p>
         </div>
-    )
+      </div>
+      <div className="comment-content-container">
+        <div className="comment-content">
+          <div className="text-style">
+            {props.commentContent.split("\n").map((line, index) => (
+              <p key={index}>{line}</p>
+            ))}
+          </div>
+        </div>
+        {props.commentImage ? (
+          <img src={props.commentImage} className="comment-img"></img>
+        ) : null}
+        <button className="like-btn">
+          <img src={likeBtnIcon} alt="" />
+        </button>
+      </div>
+    </div>
+  );
 }
