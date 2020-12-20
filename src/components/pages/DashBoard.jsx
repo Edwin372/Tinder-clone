fa7df2d8ac89d8232a588edbce13ee04b89c858e
Châu <tranhoaichau.00@gmail.com>
2020-12-20 18:16:53 +0700

    fix style:

--- src/components/pages/DashBoard.jsx
+++ src/components/pages/DashBoard.jsx
@@ -114,6 +114,6 @@ const mapStateToProps = (state) => {
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: 'posts' }
+    { collection: 'posts', orderBy: ['createdAt', 'desc'] }
   ])
 )(Dashboard)
