f984f57101ad91e51c1c608b6f78b5afb8d3bbf4
An D Van <vdan18@apcs.vn>
2021-01-12 00:19:17 +0700

    Top-trending-list

--- src/components/post/TopTrendingList.jsx
+++ src/components/post/TopTrendingList.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
-import TrendingPostSummary from './TrendingPostSummary'
-import { Link } from 'react-router-dom'
-import './TopTrendingList.scss'
-import trendingIcon from '../../svg/trendingIcon.svg'
+import React from "react";
+import TrendingPostSummary from "./TrendingPostSummary";
+import { Link } from "react-router-dom";
+import "./TopTrendingList.scss";
+import trendingIcon from "../../svg/trendingIcon.svg";
+import { firestore } from "../../config/firebaseConfig";
 
-const topTrendingList = ({posts}) => {
+const topTrendingList = ({ posts, profile }) => {
+  const viewEvent = async (postId, post) => {
+    await firestore
+      .collection("posts")
+      .doc(postId)
+      .set({
+        ...post,
+        view: post.view + 1,
+      });
+  };
   return (
     <div>
       <div className="trending-on-the-nerd">
-        <img src={trendingIcon} alt="trending-icon"/>
+        <img src={trendingIcon} alt="trending-icon" />
         <label>Trending on The Nerd</label>
       </div>
       <div className="trend-post-list-container">
-        { posts && posts.map(post => {
-          return (
-            <Link to={'/post/' + post.id} key={post.id}>
-              <TrendingPostSummary post={post} />
-            </Link>
-          )
-        })}  
+        {posts &&
+          posts.map((post) => {
+            return (
+              <Link
+                to={{
+                  pathname: "/post/" + post.id,
+                  state: {
+                    post,
+                    profile,
+                  },
+                }}
+                onClick={() => viewEvent(post.id, post)}
+              >
+                <TrendingPostSummary post={post} />
+              </Link>
+            );
+          })}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default topTrendingList
+export default topTrendingList;
