133952c6828ed9e93088f7abf481550a733bf9b0
An D Van <vdan18@apcs.vn>
2021-01-11 20:08:53 +0700

    like post

--- src/components/layout/ReadingContent.jsx
+++ src/components/layout/ReadingContent.jsx
@@ -1,85 +1,179 @@
 import React, { useState, useEffect } from "react";
 
-import EditorJS from 'react-editor-js'
+import EditorJS from "react-editor-js";
 import "./ReadingContent.scss";
-import SaveBtn from '../buttons/SaveButton.jsx'
-import ImageTool from '@editorjs/image';
-import CodeTool from '@editorjs/code';
-import Underline from '@editorjs/underline';
-import InlineCode from '@editorjs/inline-code';
-import Header from '@editorjs/header'
-import List from '@editorjs/list';
+import SaveBtn from "../buttons/SaveButton.jsx";
+import ImageTool from "@editorjs/image";
+import CodeTool from "@editorjs/code";
+import Underline from "@editorjs/underline";
+import InlineCode from "@editorjs/inline-code";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
 import defaultAvatar from "../../images/defaultAvatar.png";
-import { connect } from 'react-redux'
-import moment from 'moment';
-import { compose } from 'redux'
-import { firestoreConnect } from 'react-redux-firebase'
+import { connect } from "react-redux";
+import moment from "moment";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
+import LikeButton from "../buttons/Likebutton.jsx";
 
 const editorJsTools = {
   code: CodeTool,
   underline: Underline,
   paragraph: {
-      config: {
-        placeholder: 'Tell your story...'
-      }
+    config: {
+      placeholder: "Tell your story...",
+    },
   },
   inlineCode: {
-      class: InlineCode,
-      shortcut: 'CMD+SHIFT+M',
+    class: InlineCode,
+    shortcut: "CMD+SHIFT+M",
   },
   list: {
-      class: List,
-      inlineToolbar: true,
+    class: List,
+    inlineToolbar: true,
   },
   header: Header,
   image: {
-      class: ImageTool,
-  }
-}
-
+    class: ImageTool,
+  },
+};
 
 const ReadingContent = (props) => {
-  const {  post, firestore  } = props;
+  const { post, firestore } = props;
   const [save, saveToggle] = useState(false);
   const [authorProfile, setAuthorProfile] = useState({});
-  useEffect( () => {
+  const [likedId, setLikedId] = useState("");
+  const [likeCount, setLikeCount] = useState(0);
+  useEffect(() => {
+    const { post } = props;
+
     async function fetchData() {
       const response = await firestore
-      .collection("users")
-      .doc(post.userId)
-      .get()
-      const profile = {...response.data(), id: response.id}
-      setAuthorProfile(profile)
+        .collection("users")
+        .doc(post.userId)
+        .get();
+      const profile = { ...response.data(), id: response.id };
+      setAuthorProfile(profile);
+      await firestore
+        .collection("posts")
+        .doc(props.post.id)
+        .onSnapshot((querySnapshot) => {
+          var post = [];
+          setLikeCount(querySnapshot.data().likes);
+        });
     }
+    fetchLike(post.id);
     fetchData();
   }, []);
+  const increaseLike = async (postId, post) => {
+    const newLikeCount = likeCount + 1;
+    await firestore
+      .collection("posts")
+      .doc(postId)
+      .set({
+        ...post,
+        likes: newLikeCount,
+      });
+  };
+  const decreaseLike = async (postId, post) => {
+    const newLikeCount = likeCount - 1;
+
+    await firestore
+      .collection("posts")
+      .doc(postId)
+      .set({
+        ...post,
+        likes: newLikeCount,
+      });
+  };
+  const handleLike = async (postId, post) => {
+    await firestore
+      .collection("likes")
+      .add({ userId: props.auth.uid, contentId: postId })
+      .then((data) => {
+        setLikedId(data.id);
+        increaseLike(postId, post);
+      });
+  };
+  const handleUnlike = async (likedId, postId, post) => {
+    await firestore
+      .collection("likes")
+      .doc(likedId)
+      .delete()
+      .then((data) => {
+        setLikedId("");
+        decreaseLike(postId, post);
+      });
+  };
+  const fetchLike = async (postId) => {
+    const { firestore } = props;
+    var rawData = await firestore
+      .collection("likes")
+      .where("userId", "==", props.auth.uid)
+      .where("contentId", "==", postId)
+      .get();
+    rawData.forEach((data) => {
+      setLikedId(data.id);
+    });
+  };
   return (
-    <div id="reading-content-container">
-      <div id="reading-content-header">
-        <SaveBtn save={save} saveToggle={saveToggle}/>
-        <div id="author-info">
-          <img id="author-ava" src={authorProfile.avatar || defaultAvatar} alt="avatar"></img>
-          <div id="author-name-date">
-            <a href={`/profile/${authorProfile.id}`} id="author-name">{post.author}</a>
-            <p id="post-date">{moment(post.createdAt).format('MMMM Do, YYYY')}</p>
+    <div id="reading-content-and-like">
+      <LikeButton
+        id="like-button"
+        like={likedId ? true : false}
+        likeCount={likeCount}
+        onClick={() => {
+          likedId
+            ? handleUnlike(likedId, post.id, post)
+            : handleLike(post.id, post);
+        }}
+        imageStyle={{
+          width: "50px",
+          height: "auto",
+        }}
+        likeButtonStyle={{
+          position: "fixed",
+          top: "400px",
+          right: "200px",
+        }}
+        likeText={{
+          fontSize: "40px",
+          marginTop: "18px",
+        }}
+      />
+      <div id="reading-content-container">
+        <div id="reading-content-header">
+          <SaveBtn save={save} saveToggle={saveToggle} />
+          <div id="author-info">
+            <img
+              id="author-ava"
+              src={authorProfile.avatar || defaultAvatar}
+              alt="avatar"
+            ></img>
+            <div id="author-name-date">
+              <a href={`/profile/${authorProfile.id}`} id="author-name">
+                {post.author}
+              </a>
+              <p id="post-date">
+                {moment(post.createdAt).format("MMMM Do, YYYY")}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div id="reading-content-preheader">
-        <p id="reading-content-title">{post.title}</p>
-        <p id="reading-content-subtitle">{post.subtitle}</p>
-      </div>
-      {post.titleImage
-        ? <img
-          className="reading-content-image"
-          src={post.titleImage}
-          alt="title"
-        />
-        : null
-      }
-      <div id="reading-content-body">
-          <EditorJS 
-            id="editorjs" 
+        <div id="reading-content-preheader">
+          <p id="reading-content-title">{post.title}</p>
+          <p id="reading-content-subtitle">{post.subtitle}</p>
+        </div>
+        {post.titleImage ? (
+          <img
+            className="reading-content-image"
+            src={post.titleImage}
+            alt="title"
+          />
+        ) : null}
+        <div id="reading-content-body">
+          <EditorJS
+            id="editorjs"
             holder="editorjs"
             data={post.postContentData}
             tools={editorJsTools}
@@ -87,20 +181,19 @@ const ReadingContent = (props) => {
           >
             <div id="editorjs" />
           </EditorJS>
+        </div>
       </div>
     </div>
-    
   );
 };
 
-
 const mapStateToProps = (state) => {
-  return{
+  return {
     auth: state.firebase.auth,
-  }
-}
+  };
+};
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect() 
-)(ReadingContent)
+  firestoreConnect()
+)(ReadingContent);
--- src/components/pages/ReadPostPage.jsx
+++ src/components/pages/ReadPostPage.jsx
@@ -1,17 +1,32 @@
-import React, { Component } from 'react'
-import NavBar from '../layout/NavBar.jsx'
-import ReadingContent from '../layout/ReadingContent.jsx'
-import Commentbox from '../comments/Commentbox.jsx'
+import React, { Component } from "react";
+import NavBar from "../layout/NavBar.jsx";
+import ReadingContent from "../layout/ReadingContent.jsx";
+import Commentbox from "../comments/Commentbox.jsx";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
 
-export default class ReadPostPage extends Component {
-    render() {
-        const {post} = this.props.location.state
-        return (
-            <div id="read-post-page">
-                <NavBar/>
-                <ReadingContent post={post}/>
-                <Commentbox style={{zIndex: 10}} contentId={post.id}/>
-            </div>
-        )
-    }
+export class ReadPostPage extends Component {
+  render() {
+    const { post } = this.props.location.state;
+    return (
+      <div id="read-post-page-and-like">
+        <div id="read-post-page">
+          <NavBar />
+          <ReadingContent post={post} />
+          <Commentbox style={{ zIndex: 10 }} contentId={post.id} />
+        </div>
+      </div>
+    );
+  }
 }
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect()
+)(ReadPostPage);
